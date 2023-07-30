c45239bc986bf32f4ba01f8b5953c6961e9e69bf
Diego Altamirano <diego.altamirano@fotograf.de>
2023-07-30 11:58:17 +0200

    Add controller images

--- src/Service/EventListener/hudButtonsEventListener.ts
+++ src/Service/EventListener/hudButtonsEventListener.ts
@@ -13,18 +13,22 @@ export default function listenHudButtonEvents(player: Player): void {
   player.addTouchController(controller);
 
   leftControllerButton.addEventListener('touchstart', () => {
+    leftControllerButton.style.opacity = '50%';
     leftKey.isDown = true;
   });
 
   leftControllerButton.addEventListener('touchend', () => {
+    leftControllerButton.style.opacity = '100%';
     leftKey.isDown = false;
   });
 
   rightControllerButton.addEventListener('touchstart', () => {
+    rightControllerButton.style.opacity = '50%';
     rightKey.isDown = true;
   });
 
   rightControllerButton.addEventListener('touchend', () => {
+    rightControllerButton.style.opacity = '100%';
     rightKey.isDown = false;
   });
 }
