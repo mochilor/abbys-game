906ab38599d0e4a5d5798758cd9686c0a07ffb39
Diego Altamirano <diegutor@gmail.com>
2023-09-09 12:17:08 +0200

    Loading bar

--- src/Scene/Preload/Preload.ts
+++ src/Scene/Preload/Preload.ts
@@ -36,12 +36,20 @@ import doorSamplePath from 'url:../../../assets/audio/door.ogg';
 import titleMusic from 'url:../../../assets/audio/music/title.ogg';
 import endingMusic from 'url:../../../assets/audio/music/ending.ogg';
 
+// preload functions
+import progressBar from './ProgressBar';
+
 export default class Preload extends Phaser.Scene {
   constructor() {
     super({ key: 'Preload' });
   }
 
   public preload(): void {
+    // dist folder local path (workaround to load xml file path correctly here)
+    // thanks to parcel-reporter-static-files-copy plugin
+    this.load.bitmapFont('font', font, './xml/font.xml');
+    this.load.bitmapFont('smallFont', smallFont, './xml/small-font.xml');
+
     this.load.image('tilesetImage', tilesetPath);
     this.load.image('spearImage', spearImagePath);
     this.load.image('ballImage', ballImagePath);
@@ -82,16 +90,13 @@ export default class Preload extends Phaser.Scene {
     graphics.fillRect(0, 0, 2, 2);
     graphics.generateTexture('lightTexture', 2, 1);
 
-    // dist folder local path (workaround to load xml file path correctly here)
-    // thanks to parcel-reporter-static-files-copy plugin
-    this.load.bitmapFont('font', font, './xml/font.xml');
-    this.load.bitmapFont('smallFont', smallFont, './xml/small-font.xml');
-
     mapFiles().forEach((element: string[]) => {
       // dist folder local path
       const file = `./maps/${element[1]}`;
       this.load.tilemapTiledJSON(element[0], file);
     });
+
+    progressBar(this);
 
     // this.load.pack();
   }
---
+++ src/Scene/Preload/ProgressBar.ts
@@ -0,0 +1,56 @@
+import config from '../../../config/config.json';
+
+export default function progressBar(scene: Phaser.Scene): void {
+  const boxWidth = 75;
+  const boxOriginX = (config.gameWidth / 2) - (boxWidth / 2);
+
+  const boxHeight = 8;
+  const boxOriginY = (config.gameHeight / 2) - (boxHeight / 2);
+
+  const boxBorderWidth = 1;
+
+  const box = scene.add.graphics();
+  box.fillStyle(0x222222);
+  box.fillRect(
+    boxOriginX,
+    boxOriginY,
+    boxWidth,
+    boxHeight,
+  );
+
+  const bar = scene.add.graphics();
+  let loadingText = null;
+
+  scene.load.on('progress', (value: number) => {
+    if (loadingText === null) {
+      try {
+        loadingText = scene.add.bitmapText(
+          config.gameWidth / 2,
+          (config.gameHeight / 2) - (boxHeight),
+          'smallFont',
+          'LOADING...',
+        );
+        loadingText.setOrigin(0.5, 0.5);
+      } catch (error) {
+        // The font was not loaded yet, no problem
+      }
+    }
+
+    const barWidth = boxWidth - (boxBorderWidth * 2);
+    const barHeight = boxHeight - (boxBorderWidth * 2);
+
+    bar.clear();
+    bar.fillStyle(0xffffff, 0.5);
+    bar.fillRect(
+      boxOriginX + boxBorderWidth,
+      boxOriginY + boxBorderWidth,
+      barWidth * value,
+      barHeight,
+    );
+  });
+
+  scene.load.on('complete', () => {
+    bar.destroy();
+    box.destroy();
+  });
+}
