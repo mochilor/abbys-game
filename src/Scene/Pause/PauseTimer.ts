ff2f1647152a227dc9f17c610bc6838e652db45b
Diego Altamirano <diegutor@gmail.com>
2023-09-09 11:29:02 +0200

    Fix pause timer: use static object to store callbacks and values to make it accessible when reaching a new room

--- src/Scene/Pause/PauseTimer.ts
+++ src/Scene/Pause/PauseTimer.ts
@@ -1,43 +1,55 @@
 import { PauseTimeCounter } from './types';
 
-let init: boolean = null;
+const timer = {
+  init: false,
+  lastTime: 0,
+  pausedAt: 0,
+  gameResumed: false,
+  pauseTime: 0,
+
+  paused(): void {
+    this.pausedAt = this.lastTime;
+  },
+
+  resumed(): void {
+    this.gameResumed = true;
+  },
+
+  reset(): void {
+    this.lastTime = 0;
+    this.pausedAt = 0;
+    this.gameResumed = false;
+    this.pauseTime = 0;
+    this.init = true;
+  },
+};
 
 export default function makePauseTimer(scene: Phaser.Scene): PauseTimeCounter {
-  let lastTime: number = 0;
-
-  let pausedAt: number = 0;
-
-  let gameResumed: boolean = false;
-
-  let pauseTime: number = 0;
-
-  function paused(): void {
-    pausedAt = lastTime;
+  // We need to use a static object for the callback and values, otherwise the listener being
+  // created in a room is still in memory but not accesible.
+  if (!timer.init) {
+    scene.events.on('pause', timer.paused, timer);
+    scene.events.on('resume', timer.resumed, timer);
+    timer.init = true;
   }
 
-  function resumed(): void {
-    gameResumed = true;
-  }
+  timer.reset();
 
-  if (!init) {
-    scene.events.on('pause', paused);
-    scene.events.on('resume', resumed);
-    init = true;
+  // This function calculates the time spent on pause menu to prevent weird behaviors in some
+  // Sprites that use time, like SpikePlatform
+  function calculatePauseTime(realTime: integer): integer {
+    timer.lastTime = realTime;
+
+    if (timer.gameResumed) {
+      timer.pauseTime += realTime - timer.pausedAt;
+      timer.pausedAt = 0;
+      timer.gameResumed = false;
+    }
+
+    return timer.pauseTime;
   }
 
   return {
-    calculatePauseTime(realTime: integer): integer {
-      lastTime = realTime;
-
-      if (gameResumed) {
-        pauseTime += realTime - pausedAt;
-        pausedAt = 0;
-        gameResumed = false;
-      }
-
-      // We need to substract any time spent on pause menu ot prevent weird behaviors in some
-      // Sprites that use time, like SpikePlatform
-      return pauseTime;
-    },
+    calculatePauseTime,
   };
 }
